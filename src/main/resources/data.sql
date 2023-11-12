f2ae8c444865010b6ea6021d4bfd425706ef7d72
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-12 15:49:52 +0100

    cleaning of database values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,22 @@
-INSERT INTO roles(rolename) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+INSERT INTO roles(rolename)
+VALUES ('ROLE_USER'), ('ROLE_ADMIN');
 
 -- USERS DATABASE
-INSERT INTO users(username, password) VALUES ('username', 'password');
+INSERT INTO users(username, password)
+VALUES ('username', 'password');
 -- TODO: bcrypt password opzoeken en copy paste ^
 
 -- ACCOUNTS DATABASE
-INSERT INTO accounts(firstname, lastname, phonenumber, emailaddress) VALUES ('marianne', 'test', '0611122333', 'marianne@test.com');
-INSERT INTO accounts(firstname, lastname, phonenumber, emailaddress) VALUES ('koen', 'banaan', '0611122333', 'koen@banaan.com');
+INSERT INTO accounts(firstname, lastname, phonenumber, emailaddress)
+VALUES ('marianne', 'test', '0611122333', 'marianne@test.com'),
+       ('koen', 'banaan', '0611122333', 'koen@banaan.com');
+
 
 -- PROPERTIES DATABASE
-INSERT INTO properties(address, price, description) VALUES ('Singel 10', 725000.00, 'Aliquam eu lorem libero. In pharetra blandit ligula, non volutpat quam hendrerit non. Nulla et diam lorem. Etiam ut tortor luctus magna tempor hendrerit quis.');
-INSERT INTO properties(address, price, description) VALUES ('Kerkstraat 125', 450000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pretium metus, vitae elementum ante consectetur sit amet. Nulla quis bibendum nibh. Proin vitae erat.');
-INSERT INTO properties(address, price, description) VALUES ('Singel 10', 725000.00, 'Aliquam eu lorem libero. In pharetra blandit ligula, non volutpat quam hendrerit non. Nulla et diam lorem. Etiam ut tortor luctus magna tempor hendrerit quis.');
-INSERT INTO properties(address, price, description) VALUES ('Kerkstraat 125', 450000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pretium metus, vitae elementum ante consectetur sit amet. Nulla quis bibendum nibh. Proin vitae erat.');
+INSERT INTO properties(address, price, description)
+VALUES ('Singel 10', 725000.00, 'Aliquam eu lorem libero. In pharetra blandit ligula, non volutpat quam hendrerit non. Nulla et diam lorem. Etiam ut tortor luctus magna tempor hendrerit quis.'),
+       ('Kerkstraat 125', 450000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pretium metus, vitae elementum ante consectetur sit amet. Nulla quis bibendum nibh. Proin vitae erat.'),
+       ('Singel 10', 725000.00, 'Aliquam eu lorem libero. In pharetra blandit ligula, non volutpat quam hendrerit non. Nulla et diam lorem. Etiam ut tortor luctus magna tempor hendrerit quis.'),
+       ('Kerkstraat 125', 450000.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet pretium metus, vitae elementum ante consectetur sit amet. Nulla quis bibendum nibh. Proin vitae erat.');
 
 -- VIEWINGS DATABASE
